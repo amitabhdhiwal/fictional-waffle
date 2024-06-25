8c349debb96c17aec20df3d14115cc0854c6d01d
Amitabh Dhiwal <amitabh@dhiwal.com>
2024-06-25 22:46:01 +0530

    update tests

--- api/src/modules/keys/keys.service.spec.js
+++ api/src/modules/keys/keys.service.spec.js
@@ -9,7 +9,8 @@ describe('KeyService', () => {
   let app;
   let keys;
 
-  let customerId = '123456';
+  //random number between 1000 and 9999
+  const id = () => Math.floor(Math.random() * 9999) + 1000;
 
   beforeAll(async () => {
     keys = await Test.createTestingModule({
@@ -30,7 +31,7 @@ describe('KeyService', () => {
   describe('isValidRequest', () => {
     it('should return true for valid request', async () => {
       const expiresAt = new Date().getTime() + 600000;
-      let data = { customerId, expiresAt, rateLimit: 100, enabled: 1 }
+      let data = { customerId : id(), expiresAt, rateLimit: 100, enabled: 1 }
       const keysController = keys.get(KeysController);
       const res = await keysController.createKey(data);
       let key = res.key;
@@ -38,12 +39,12 @@ describe('KeyService', () => {
       let validRequestData = { key }
       const keyService = keys.get(KeyService);
       const isValidRequest = await keyService.isValidRequest(validRequestData);
-      expect(isValidRequest).toEqual(true);
+      expect(isValidRequest.valid).toEqual(true);
     });
 
     it('should return false for rate limited exceeded', async () => {
       const expiresAt = new Date().getTime() + 600000;
-      let data = { customerId, expiresAt, rateLimit: 100, enabled: 1 }
+      let data = { customerId : id(), expiresAt, rateLimit: 100, enabled: 1 }
       const keysController = keys.get(KeysController);
       const res = await keysController.createKey(data);
       let key = res.key;
@@ -55,20 +56,20 @@ describe('KeyService', () => {
 
       let validRequestData = { key }
       const isValidRequest = await keyService.isValidRequest(validRequestData);
-      expect(isValidRequest).toEqual(false);
+      expect(isValidRequest.valid).toEqual(false);
     });
 
     //return false for expired key
     it('should return false for expired key', async () => {
       const expiresAt = new Date().getTime() - 600000;
-      let data = { customerId, expiresAt, rateLimit: 100, enabled: 1 }
+      let data = { customerId : id(), expiresAt, rateLimit: 100, enabled: 1 }
       const keysController = keys.get(KeysController);
       const res = await keysController.createKey(data);
       let key = res.key;
       let validRequestData = { key }
       const keyService = keys.get(KeyService);
       const isValidRequest = await keyService.isValidRequest(validRequestData);
-      expect(isValidRequest).toEqual(false);
+      expect(isValidRequest.valid).toEqual(false);
     });
   });
 
